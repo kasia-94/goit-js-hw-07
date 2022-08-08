e67c5dae6fdd9c645bcf39612d479482b0e72b84
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-08 21:41:05 +0300

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCards(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+           <a class="gallery__item" href="${original}">
+      <img class = 'gallery__image' src="${preview}"
+      data-source="${original}" 
+      alt="${description}"/>
+      </a>`;
+    })
+    .join("");
+}
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  showCounter: false,
+  overlayOpacity: 0.9,
+});
