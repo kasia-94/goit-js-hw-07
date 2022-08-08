59339901b631bc70c4a3237f35404ef91e805cbf
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-08 20:57:49 +0300

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCards(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+paletteContainer.addEventListener("click", onPaletteContainerClick);
+
+function createGalleryCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <div class = "gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class = 'gallery__image' src="${preview}"
+      data-source="${original}" 
+      alt="${description}"/>
+      </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onPaletteContainerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const selectedImage = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}">
+`);
+  instance.show();
+  window.addEventListener("keydown", onEscapePress);
+}
+function onCloseModalWithEscape() {
+  window.removeEventListener("keydown", onEscapePress);
+}
+
+function onEscapePress(evt) {
+  if (evt.code === "Escape") {
+    onCloseModalWithEscape();
+    const modal = document.querySelector(".basicLightbox");
+    modal.remove(".basicLightbox");
+  }
+}
